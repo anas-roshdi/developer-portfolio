edda913659eb5879ebfbafcd4db7db18fb3da2e5
anas-roshdi <a7nas.talika@gmail.com>
2026-04-06 15:04:46 +0300

    add new project

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,6 +1,15 @@
 export const projectsData = [
-  {
-        id: 1,
+    {
+      id: 1,
+      name: 'Devo - Unified Business Management System',
+      description: "Developed a professional desktop application for accounting and inventory management. The system features a dynamic invoicing module for sales and purchases, integrated with a local SQLite database. It includes automated financial reporting and data export capabilities to Excel using Pandas.",
+      tools: ['Python', 'Tkinter', 'SQLite', 'Pandas', 'Git'],
+      role: 'Software Engineer',
+      code: '', 
+      demo: '', 
+    },
+    {
+        id: 2,
         name: 'Express Book Reviews API',
         description: "Built a robust RESTful API for book reviews. I implemented JWT for secure authentication and session-based access control. The project focused on CRUD operations and optimizing backend performance using Node.js and Express.",
         tools: ['Node.js', 'Express', 'JWT', 'Postman', 'JavaScript'],
@@ -9,7 +18,7 @@ export const projectsData = [
         demo: '',
     },
     {
-        id: 2,
+        id: 3,
         name: 'Smart Attendance System (NFC)',
         description: "My Graduation Project: Developed an automated attendance system using NFC technology and Java. The system streamlines the process of tracking student attendance with high accuracy and provides real-time data for school administration.",
         tools: ['Java', 'NFC Technology', 'SQL Server', 'Android Studio'],
@@ -18,7 +27,7 @@ export const projectsData = [
         demo: '',
     },
     {
-        id: 3,
+        id: 4,
         name: 'Arabic Character Recognition',
         description: "Designed a Machine Learning model to recognize handwritten Arabic characters. I utilized Deep Learning techniques and the VGG16 architecture to achieve high classification accuracy.",
         tools: ['Python', 'TensorFlow', 'Keras', 'Computer Vision'],
@@ -27,7 +36,7 @@ export const projectsData = [
         demo: '',
     },
     {
-        id: 4,
+        id: 5,
         name: 'E-Plant Shopping App',
         description: "Created a responsive E-commerce front-end application for a plant nursery. Used React and Redux Toolkit for state management, focusing on a seamless user experience and modern UI design.",
         tools: ['React', 'Redux', 'Tailwind CSS', 'Vite'],
