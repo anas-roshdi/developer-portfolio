4adf2d1a6376f3a184488ac4055d6801d070bbd1
anas-roshdi <a7nas.talika@gmail.com>
2026-04-06 15:15:03 +0300

    add links

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -5,7 +5,7 @@ export const projectsData = [
       description: "Developed a professional desktop application for accounting and inventory management. The system features a dynamic invoicing module for sales and purchases, integrated with a local SQLite database. It includes automated financial reporting and data export capabilities to Excel using Pandas.",
       tools: ['Python', 'Tkinter', 'SQLite', 'Pandas', 'Git'],
       role: 'Software Engineer',
-      code: '', 
+      code: 'https://github.com/anas-roshdi/devo', 
       demo: '', 
     },
     {
@@ -14,7 +14,7 @@ export const projectsData = [
         description: "Built a robust RESTful API for book reviews. I implemented JWT for secure authentication and session-based access control. The project focused on CRUD operations and optimizing backend performance using Node.js and Express.",
         tools: ['Node.js', 'Express', 'JWT', 'Postman', 'JavaScript'],
         role: 'Backend Developer',
-        code: '',
+        code: 'https://github.com/anas-roshdi/expressBookReviews',
         demo: '',
     },
     {
@@ -23,7 +23,7 @@ export const projectsData = [
         description: "My Graduation Project: Developed an automated attendance system using NFC technology and Java. The system streamlines the process of tracking student attendance with high accuracy and provides real-time data for school administration.",
         tools: ['Java', 'NFC Technology', 'SQL Server', 'Android Studio'],
         role: 'Lead Developer',
-        code: '',
+        code: 'https://github.com/anas-roshdi/Arabic-character-recognition',
         demo: '',
     },
     {
@@ -32,7 +32,7 @@ export const projectsData = [
         description: "Designed a Machine Learning model to recognize handwritten Arabic characters. I utilized Deep Learning techniques and the VGG16 architecture to achieve high classification accuracy.",
         tools: ['Python', 'TensorFlow', 'Keras', 'Computer Vision'],
         role: 'AI Developer',
-        code: '',
+        code: 'https://github.com/anas-roshdi/Arabic-character-recognition',
         demo: '',
     },
     {
@@ -41,7 +41,7 @@ export const projectsData = [
         description: "Created a responsive E-commerce front-end application for a plant nursery. Used React and Redux Toolkit for state management, focusing on a seamless user experience and modern UI design.",
         tools: ['React', 'Redux', 'Tailwind CSS', 'Vite'],
         role: 'Front-End Developer',
-        code: '',
+        code: 'https://github.com/anas-roshdi/e-plantShopping',
         demo: '',
     }
 ];
