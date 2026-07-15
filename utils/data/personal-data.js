03a00e6caa747d38adea3c78702ee90efcbf8741
anas-roshdi <a7nas.talika@gmail.com>
2026-07-15 17:34:26 +0300

    Update portfolio hero section and profile summary and projects

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,6 +4,7 @@ export const personalData = {
   designation: "Software Developer & Mobile App Developer",
   description: "I am a Computer Science graduate from Umm Al-Qura University with First Class Honors (GPA 3.84/4.00). I am a passionate software engineer with a strong focus on Mobile Application Development and a solid background in Machine Learning. I enjoy solving complex problems, designing intuitive user interfaces, and building secure, scalable applications.",
   phone: '+966546563094',
+  email: "[a7nas.tailka@gmail.com]",
   address: 'Makkah ',
   github: 'https://github.com/anas-roshdi',
   facebook: '',
